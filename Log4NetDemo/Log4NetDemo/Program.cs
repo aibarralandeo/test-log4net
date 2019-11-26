b316b5e6714b50b316886eb643244a3bae163676
Adolfo F. Ibarra Landeo <aibarradev@outlook.com>
2019-11-26 11:04:57 -0500

    Logger implemented / Error demo message working

--- Log4NetDemo/Log4NetDemo/Program.cs
+++ Log4NetDemo/Log4NetDemo/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace Log4NetDemo
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
+            Console.WriteLine("====================== Logger demo =====================");
+
+            log.Error("This is my error message.");
+
+            Console.ReadLine();
         }
     }
 }
